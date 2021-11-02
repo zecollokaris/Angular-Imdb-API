175a9ff50008815823134829f61712cd63408598
zecollokaris <collo.kariss@gmail.com>
2021-11-03 01:06:05 +0300

    created service for most popular movies and displayed them on html most popular component

--- src/app/main/components/most-popular-movies/most-popular-movies.component.ts
+++ src/app/main/components/most-popular-movies/most-popular-movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../domain/movie';
+import { MainService } from '../../services/main.service';
 
 @Component({
   selector: 'app-most-popular-movies',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MostPopularMoviesComponent implements OnInit {
 
-  constructor() { }
+  movies: Array<Movie> = [];
 
-  ngOnInit() {
+  constructor(private mainService: MainService) { }
+
+  async ngOnInit() {
+    this.movies = await this.mainService.getMostPopularMovies();
   }
 
 }
