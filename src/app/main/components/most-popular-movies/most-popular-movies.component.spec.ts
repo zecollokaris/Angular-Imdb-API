95aaa90974a6c15acbc19f19b043d3395e3f039f
zecollokaris <collo.kariss@gmail.com>
2021-11-03 00:51:02 +0300

    created most popular component and set up route

---
+++ src/app/main/components/most-popular-movies/most-popular-movies.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MostPopularMoviesComponent } from './most-popular-movies.component';
+
+describe('MostPopularMoviesComponent', () => {
+  let component: MostPopularMoviesComponent;
+  let fixture: ComponentFixture<MostPopularMoviesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MostPopularMoviesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MostPopularMoviesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/main/components/most-popular-movies/most-popular-movies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-most-popular-movies',
+  templateUrl: './most-popular-movies.component.html',
+  styleUrls: ['./most-popular-movies.component.css']
+})
+export class MostPopularMoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
