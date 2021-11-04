61fa5fae9eeaaa6c4bcb17cde274235e1ae9ec21
zecollokaris <collo.kariss@gmail.com>
2021-11-04 20:04:41 +0300

    created array of each movie type needed in dashboard

--- src/app/main/components/dashboard/dashboard.component.ts
+++ src/app/main/components/dashboard/dashboard.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Movie } from '../../domain/movie';
-import { MainService } from '../../services/main.service';
+import { Component, OnInit } from "@angular/core";
+import { Movie } from "../../domain/movie";
+import { MainService } from "../../services/main.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent implements OnInit {
+  comingmovies: Array<Movie> = [];
+  topmovies: Array<Movie> = [];
+  toptvs: Array<Movie> = [];
+  popmovies: Array<Movie> = [];
+  poptvs: Array<Movie> = [];
+  intheaters: Array<Movie> = [];
 
-  movies: Array<Movie> = [];
-
-  constructor(private mainService: MainService) { }
+  constructor(private mainService: MainService) {}
 
   async ngOnInit() {
-    this.movies = await 
-    this.mainService.getComingSoon();
-    // this.mainService.getTop250Movies();
-    // this.mainService.getTop250Tvs();
-    // this.mainService.getMostPopularMovies()
-    // this.mainService.getMostPopularTvs()
-    // this.mainService.getInTheaters()
+    this.comingmovies = await this.mainService.getComingSoon();
+    this.topmovies = await this.mainService.getTop250Movies();
+    this.toptvs = await this.mainService.getTop250Tvs();
+    this.popmovies = await this.mainService.getMostPopularMovies()
+    this.poptvs = await this.mainService.getMostPopularTvs()
+    this.intheaters = await this.mainService.getInTheaters()
   }
-
 }
