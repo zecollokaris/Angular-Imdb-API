3e5a2b94c08cd6fe3544bc2cec6f420b90e701c8
zecollokaris <collo.kariss@gmail.com>
2021-11-10 00:48:53 +0300

    created view details test

--- src/app/main/components/top250-tvs/top250-tvs.component.spec.ts
+++ src/app/main/components/top250-tvs/top250-tvs.component.spec.ts
@@ -2,13 +2,27 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Top250TvsComponent } from './top250-tvs.component';
 
+import { MainService } from './../../services/main.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { Movie } from '../../domain/movie';
+
 describe('Top250TvsComponent', () => {
   let component: Top250TvsComponent;
   let fixture: ComponentFixture<Top250TvsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ Top250TvsComponent ]
+      declarations: [ Top250TvsComponent ],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        RouterTestingModule
+      ],
     })
     .compileComponents();
   }));
@@ -17,5 +31,21 @@ describe('Top250TvsComponent', () => {
     fixture = TestBed.createComponent(Top250TvsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should return top 250 tvs', async () => {  
+
+    const service = fixture.debugElement.injector.get(MainService);
+
+    // Returns an item  in an array when getTop250Movies is called
+    spyOn(service,"getTop250Movies").and.returnValue([ <Movie>{
+      title: 'Top 250 Tv'
+    }]);
+
+    await component.ngOnInit();    
+
+    expect(component.movies.length).toEqual(1);
+
+    expect(component.movies[0].title).toEqual('Top 250 Tv');
   });
 });
--- src/app/main/components/viewdetails/viewdetails.component.spec.ts
+++ src/app/main/components/viewdetails/viewdetails.component.spec.ts
@@ -2,13 +2,27 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewdetailsComponent } from './viewdetails.component';
 
+import { MainService } from './../../services/main.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { Movie } from '../../domain/movie';
+
 describe('ViewdetailsComponent', () => {
   let component: ViewdetailsComponent;
   let fixture: ComponentFixture<ViewdetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewdetailsComponent ]
+      declarations: [ ViewdetailsComponent ],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        RouterTestingModule
+      ],
     })
     .compileComponents();
   }));
@@ -17,5 +31,19 @@ describe('ViewdetailsComponent', () => {
     fixture = TestBed.createComponent(ViewdetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should return view details', async () => {  
+
+    const service = fixture.debugElement.injector.get(MainService);
+
+    // Returns an item  in an array when getTop250Movies is called
+    spyOn(service,"searchResultById").and.returnValue([ <Movie>{
+      title: 'Search Result'
+    }]);
+
+    await component.ngOnInit();    
+
+    expect(component.result[0].title).toEqual('Search Result');
   });
 });
